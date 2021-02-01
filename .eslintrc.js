1b1002601fa816e924ebce6eb9b7cb8e10c80342
Dan Woodson <woodson.dan@gmail.com>
2021-01-31 22:16:47 -0700

    Turned off eslint error for missing prop validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/prop-types': 'off', // Don't warn about missing prop validation
   },
   parser: 'babel-eslint',
 }
